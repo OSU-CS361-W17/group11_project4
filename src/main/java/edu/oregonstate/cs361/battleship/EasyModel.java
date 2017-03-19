6dd7344f981511ba4a95ae9d59094aceb83dc637
RivH <hendriri@oregonstate.edu>
2017-03-19 16:55:13 -0700

    Add files via upload

---
+++ src/main/java/edu/oregonstate/cs361/battleship/EasyModel.java
@@ -0,0 +1,10 @@
+package edu.oregonstate.cs361.battleship;
+
+import edu.oregonstate.cs361.battleship.BattleshipModel;
+
+public class EasyModel extends BattleshipModel {
+    //changed or added methods or variables for easy mode are put here
+    public void  shootAtPlayer() {
+        //enter method for shooting at player for easy mode
+    }
+}
---
+++ src/main/java/edu/oregonstate/cs361/battleship/HardModel.java
@@ -0,0 +1,11 @@
+package edu.oregonstate.cs361.battleship;
+
+import edu.oregonstate.cs361.battleship.BattleshipModel;
+
+
+public class HardModel extends BattleshipModel{
+    //changed or added methods or variables for hard mode are put here
+    public void  shootAtPlayer() {
+        //enter method for shooting at player for hard mode
+    }
+}
