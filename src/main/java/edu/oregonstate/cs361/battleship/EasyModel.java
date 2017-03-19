38145e9bdb51745b5314afce5ba394baf7959e08
CaitlinBerger <bergerc@oregonstate.edu>
2017-03-19 13:27:02 -0700

    "EasyModel" & "HardModel" classes are added that inherit from BattlehshipModel New methods to fire at user must be implemented.

---
+++ src/main/java/edu/oregonstate/cs361/battleship/EasyModel.java
@@ -0,0 +1,8 @@
+import edu.oregonstate.cs361.battleship.BattleshipModel;
+
+public class EasyModel extends BattleshipModel {
+    //changed or added methods or variables for easy mode are put here
+    public void  shootAtPlayer() {
+        //enter method for shooting at player for easy mode
+    }
+}
---
+++ src/main/java/edu/oregonstate/cs361/battleship/HardModel.java
@@ -0,0 +1,8 @@
+import edu.oregonstate.cs361.battleship.BattleshipModel;
+
+public class HardModel extends BattleshipModel{
+    //changed or added methods or variables for hard mode are put here
+    public void  shootAtPlayer() {
+        //enter method for shooting at player for hard mode
+    }
+}
